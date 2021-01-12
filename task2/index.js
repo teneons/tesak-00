2f9ce6b1b40248605a3c1dbbb4d3e258d9b3b2fc
teneons <teneons@gmail.com>
2021-01-12 23:34:53 +0200

    body stylization

--- task2/index.js
+++ task2/index.js
@@ -1,7 +1,16 @@
-//* stails
+//* style
 document.body.style.boxSizing = 'border-box'
 document.body.style.margin = 0
 document.body.style.outlineStyle = 'none'
 document.body.style.outline = 'none'
 document.body.style.userSelect = 'none'
+
+//style for body
+document.body.style.width = '100vw';
+document.body.style.height = '100vh';
+document.body.style.display = 'flex';
+document.body.style.alignItems = 'center';
+document.body.style.justifyContent = 'center';
+document.body.style.flexDirection = 'column';
+document.body.style.backgroundColor = '#101010';
 
