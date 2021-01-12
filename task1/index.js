d0cf0c32eaa1dd302ee3cb76fecde0a6d75108da
teneons <teneons@gmail.com>
2021-01-12 12:45:32 +0200

    create content calc block

--- task1/index.js
+++ task1/index.js
@@ -6,15 +6,28 @@ document.body.style.outline = 'none'
 document.body.style.userSelect = 'none'
 
 //create elements
-let calcBlock = document.createElement('div')
-  calcBlock.id = 'calcBlock';
-  calcBlock.style.width = '100vw';
-  calcBlock.style.height = '100vh';
-  calcBlock.style.backgroundColor = '#999999';
+const mainBlock = document.createElement('div');   //main block
+  mainBlock.id = 'mainBlock';
+  mainBlock.style.width = '100vw';
+  mainBlock.style.height = '100vh';
+  mainBlock.style.backgroundColor = '#999999';
+  mainBlock.style.display = 'flex';
+  mainBlock.style.alignItems = 'center';
+  mainBlock.style.justifyContent = 'center';
+  mainBlock.style.flexDirection = 'column';
+
+const calcBlock = document.createElement('div');   //calc block
+  calcBlock.id = 'mainBlock';
+  calcBlock.style.width = '18vw';
+  calcBlock.style.height = '25vh';
+  calcBlock.style.border = '2px solid #000000';
   calcBlock.style.display = 'flex';
   calcBlock.style.alignItems = 'center';
   calcBlock.style.justifyContent = 'center';
+  calcBlock.style.flexDirection = 'column';
+
 
 
 //insert elements
-document.body.append(calcBlock)
+document.body.append(mainBlock)
+mainBlock.append(calcBlock)
