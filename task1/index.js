2193ec8a7972245afcf6039316fc07645150a5fc
teneons <teneons@gmail.com>
2021-01-12 10:24:01 +0200

    created main block

--- task1/index.js
+++ task1/index.js
@@ -5,3 +5,16 @@ document.body.style.outlineStyle = 'none'
 document.body.style.outline = 'none'
 document.body.style.userSelect = 'none'
 
+//create elements
+let calcBlock = document.createElement('div')
+  calcBlock.id = 'calcBlock';
+  calcBlock.style.width = '100vw';
+  calcBlock.style.height = '100vh';
+  calcBlock.style.backgroundColor = '#999999';
+  calcBlock.style.display = 'flex';
+  calcBlock.style.alignItems = 'center';
+  calcBlock.style.justifyContent = 'center';
+
+
+//insert elements
+document.body.append(calcBlock)
