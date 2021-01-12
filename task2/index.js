637cb843b7c61e70eb82080d9e090af85a9bf236
teneons <teneons@gmail.com>
2021-01-12 23:25:20 +0200

    task 2; make base

---
+++ task2/index.js
@@ -0,0 +1,7 @@
+//* stails
+document.body.style.boxSizing = 'border-box'
+document.body.style.margin = 0
+document.body.style.outlineStyle = 'none'
+document.body.style.outline = 'none'
+document.body.style.userSelect = 'none'
+
