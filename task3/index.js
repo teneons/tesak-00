4e977b5fedb63b5ee7db1ba5d0c0a082867b0a8a
teneons <teneons@gmail.com>
2021-01-13 15:15:45 +0200

    start task 3; made base add user

---
+++ task3/index.js
@@ -0,0 +1,6 @@
+//* style
+document.body.style.boxSizing = 'border-box'
+document.body.style.margin = 0
+document.body.style.outlineStyle = 'none'
+document.body.style.outline = 'none'
+document.body.style.userSelect = 'none'
