5e12372fbc21d62b9fe07cb6f6f81cdd7a32b500
teneons <teneons@gmail.com>
2021-01-14 21:06:51 +0200

    cent count changed; UI increased

--- task1/index.js
+++ task1/index.js
@@ -18,8 +18,8 @@ const mainBlock = document.createElement('div');   //main block
 
 const calcBlock = document.createElement('div');   //calc block
   calcBlock.id = 'mainBlock';
-  calcBlock.style.width = '18vw';
-  calcBlock.style.height = '27vh';
+  calcBlock.style.width = '20vw';
+  calcBlock.style.height = '30vh';
   calcBlock.style.border = '2px solid #000000';
   calcBlock.style.display = 'flex';
   calcBlock.style.alignItems = 'center';
@@ -37,7 +37,6 @@ const fieldSum = document.createElement('input');    //sum
   fieldSum.style.border = '0px solid';
   fieldSum.style.outline = 'none';
   fieldSum.style.textAlign = 'center';
-
 
 const fieldPrice = document.createElement('input');   //price
   fieldPrice.type = 'number';
@@ -71,7 +70,7 @@ const hr = document.createElement('hr');    //hr line
 const fieldСhange = document.createElement('span');
   fieldСhange.id = 'fieldСhange';
   fieldСhange.style.width = '100%';
-  fieldСhange.style.fontSize = '2vh';
+  fieldСhange.style.fontSize = '2.5vh';
   fieldСhange.style.textAlign = 'center';
   fieldСhange.style.fontWeight = 'bold';
 
@@ -90,8 +89,33 @@ calcBlock.append(fieldСhange);
 btnCount.addEventListener('click', () => {
   let change = ((fieldSum.value * 100 - fieldPrice.value * 100)/100).toFixed(2);   //difference
   let dollar = Math.trunc(change);
-  let cent = (change * 100 - dollar * 100)/100; //exact cent values
+  let cent = ((change * 100 - dollar * 100)/100)*100; //exact cent values
 
-  fieldСhange.innerHTML = `Your change ${change}$ or ${dollar}$ ${cent*100}¢`;   //out
+  //1 5 10 25 50 cents
+  let centChange = cent;
+  let arrCountCents = [];
+
+  if(Math.trunc(centChange / 50) != 0) {
+    arrCountCents.push(`50¢x${Math.trunc(centChange / 50)}`);
+    centChange -= 50;
+  }
+  if(Math.trunc(centChange / 25) != 0) {
+    arrCountCents.push(`25¢x${Math.trunc(centChange / 25)}`)
+    centChange -=25;
+  }
+  if(Math.trunc(centChange / 10) != 0) {
+    arrCountCents.push(`10¢x${Math.trunc(centChange / 10)}`);
+    centChange -=10;
+  }
+  if(Math.trunc(centChange / 5) != 0) {
+    arrCountCents.push(`5¢x${Math.trunc(centChange / 5)}`);
+    centChange -=5;
+  }
+  if(Math.trunc(centChange / 1) != 0) {
+    arrCountCents.push(`1¢x${Math.trunc(centChange / 1)}`);
+    centChange -= 1;
+  }
+
+  fieldСhange.innerHTML = `Your change ${change}$ or ${dollar}$ ${arrCountCents}`;   //out
 
 })
